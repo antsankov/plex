97f29ba82237f92114d062e93dab76b4d0db18ff
chrismin <chrismin@users.noreply.github.com>
2018-05-21 18:29:13 -0700

    build: set up Sentry (#132)

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,8 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 import { AppRouter } from "./router/router";
 import registerServiceWorker from "./registerServiceWorker";
+import * as Raven from "raven-js";
+import * as createRavenMiddleware from "raven-for-redux";
 import "bootstrap/dist/css/bootstrap.css";
 import "font-awesome/css/font-awesome.css";
 import "./assets/css/index.css";
@@ -9,11 +11,27 @@ import "font-awesome/css/font-awesome.css";
 
 import { DebtEntity, loadState, saveState } from "./models";
 
-import { createStore, applyMiddleware, compose } from "redux";
+import { applyMiddleware, compose, createStore, Middleware } from "redux";
 import { Provider } from "react-redux";
 import { reducers } from "./reducers";
 import reduxThunk from "redux-thunk";
 const throttle = require("lodash/throttle");
+
+function setUpMiddleware(): Middleware[] {
+    let middlewareArray = [reduxThunk];
+
+    const sentryDsn = process.env.REACT_APP_PLEX_SENTRY_DSN;
+
+    if (process.env.NODE_ENV !== "production" || !sentryDsn) {
+        return middlewareArray;
+    }
+
+    Raven.config(sentryDsn).install();
+
+    const ravenMiddleware = createRavenMiddleware(Raven);
+
+    return [...middlewareArray, ravenMiddleware];
+}
 
 const devToolsKey: string = "devToolsExtension";
 
@@ -21,11 +39,13 @@ const persistedState = loadState();
 
 const env = process.env.NODE_ENV || "unknown";
 
+const middlewares: Middleware[] = setUpMiddleware();
+
 let store = createStore(
     reducers,
     persistedState,
     compose(
-        applyMiddleware(reduxThunk),
+        applyMiddleware(...middlewares),
         window[devToolsKey] ? window[devToolsKey]() : (f: any) => f,
     ),
 );
