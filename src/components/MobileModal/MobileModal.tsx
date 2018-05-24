c08479ef1846c5c1960f36396fb187c3d5db4297
Kayvon Tehranian <ktehranian@gmail.com>
2018-05-24 18:01:23 -0400

    feat: introduce MobileModal react component

---
+++ src/components/MobileModal/MobileModal.tsx
@@ -0,0 +1,66 @@
+import * as React from "react";
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from "reactstrap";
+
+import { A } from "../../components/StyledComponents";
+
+interface Props {}
+
+interface State {
+    showModal: boolean;
+}
+
+class MobileModal extends React.Component<Props, State> {
+    constructor(props: Props) {
+        super(props);
+        this.toggle = this.toggle.bind(this);
+        this.state = {
+            showModal: false,
+        };
+    }
+
+    toggle() {
+        this.setState({ showModal: !this.state.showModal });
+    }
+
+    render() {
+        const content = (
+            <span>
+                Dharma Plex is a web3-enabled application. You will need to use one of the below
+                web3-enabled browsers in order to properly access this site.
+                <ul>
+                    <li>
+                        <A href="https://www.toshi.org/" target="_blank">
+                            Toshi
+                        </A>
+                    </li>
+                    <li>
+                        <A href="https://www.cipherbrowser.com/" target="_blank">
+                            Cipher
+                        </A>
+                    </li>
+                    <li>
+                        <A href="https://trustwalletapp.com/" target="_blank">
+                            Trust Wallet
+                        </A>
+                    </li>
+                </ul>
+            </span>
+        );
+
+        return (
+            <div>
+                <Modal isOpen={this.state.showModal} toggle={this.toggle}>
+                    <ModalHeader toggle={this.toggle}>Your browser isn't web3 enabled</ModalHeader>
+                    <ModalBody>{content}</ModalBody>
+                    <ModalFooter>
+                        <Button color="primary" onClick={this.toggle}>
+                            OK
+                        </Button>
+                    </ModalFooter>
+                </Modal>
+            </div>
+        );
+    }
+}
+
+export { MobileModal };
