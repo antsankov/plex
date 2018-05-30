126d9b2df0be40d71591b6e0f132a4da59ff6ccf
Kayvon Tehranian <ktehranian@gmail.com>
2018-05-30 16:37:53 -0700

    feat: detect mobile device and show relevant browsers accordingly

--- src/components/MobileModal/MobileModal.tsx
+++ src/components/MobileModal/MobileModal.tsx
@@ -1,9 +1,5 @@
 import * as React from "react";
-import {
-    Web3BrowserIconWrapper,
-    Web3BrowserIconWrapperLeft,
-    Web3BrowserIcon,
-} from "./styledComponents";
+import { Web3BrowserIcon, Web3BrowserIcons, Web3BrowserIconWrapper } from "./styledComponents";
 import { PlexModal } from "../PlexModal";
 
 interface Props {
@@ -23,22 +19,56 @@ class MobileModal extends React.Component<Props, State> {
         };
     }
 
-    componentDidMount() {
-        if (this.props.showWeb3BrowserModal) {
-            this.setState({ showModal: true });
-        }
-    }
-
     componentWillReceiveProps(nextProps: Props) {
         if (nextProps.showWeb3BrowserModal) {
             this.setState({ showModal: true });
         }
     }
 
+    isMobileDevice(): boolean {
+        return (
+            typeof window.orientation !== "undefined" ||
+            navigator.userAgent.indexOf("IEMobile") !== -1
+        );
+    }
+
     render() {
         const toshi = require("../../assets/img/toshi.png");
         const trustWallet = require("../../assets/img/trust_wallet.png");
         const metamask = require("../../assets/img/metamask.png");
+        const brave = require("../../assets/img/brave.png");
+
+        const mobileBrowsers = (
+            <div>
+                <Web3BrowserIconWrapper>
+                    <a href="https://www.toshi.org/" target="_blank">
+                        <Web3BrowserIcon src={toshi} alt="Toshi" />
+                    </a>
+                </Web3BrowserIconWrapper>
+                <Web3BrowserIconWrapper>
+                    <a href="https://trustwalletapp.com/" target="_blank">
+                        <Web3BrowserIcon src={trustWallet} alt="Trust Wallet" />
+                    </a>
+                </Web3BrowserIconWrapper>
+            </div>
+        );
+
+        const desktopBrowers = (
+            <div>
+                <Web3BrowserIconWrapper>
+                    <a href="https://metamask.io/" target="_blank">
+                        <Web3BrowserIcon src={metamask} alt="MetaMask" />
+                    </a>
+                </Web3BrowserIconWrapper>
+                <Web3BrowserIconWrapper>
+                    <a href="https://brave.com/" target="_blank">
+                        <Web3BrowserIcon src={brave} alt="Brave" />
+                    </a>
+                </Web3BrowserIconWrapper>
+            </div>
+        );
+
+        const browserIcons = this.isMobileDevice() ? mobileBrowsers : desktopBrowers;
 
         const content = (
             <div>
@@ -50,29 +80,7 @@ class MobileModal extends React.Component<Props, State> {
                     You’ll need to use one of the below Web3-enabled browsers in order to properly
                     interact with this site:
                 </p>
-
-                <h3>#desktop</h3>
-                <div>
-                    <Web3BrowserIconWrapperLeft>
-                        <a href="https://www.metamask.org/" target="_blank">
-                            <Web3BrowserIcon src={metamask} alt="metamask" />
-                        </a>
-                    </Web3BrowserIconWrapperLeft>
-                </div>
-
-                <h3>#mobile</h3>
-                <div>
-                    <Web3BrowserIconWrapperLeft>
-                        <a href="https://www.toshi.org/" target="_blank">
-                            <Web3BrowserIcon src={toshi} alt="Toshi" />
-                        </a>
-                    </Web3BrowserIconWrapperLeft>
-                    <Web3BrowserIconWrapper>
-                        <a href="https://trustwalletapp.com/" target="_blank">
-                            <Web3BrowserIcon src={trustWallet} alt="Trust Wallet" />
-                        </a>
-                    </Web3BrowserIconWrapper>
-                </div>
+                <Web3BrowserIcons>{browserIcons}</Web3BrowserIcons>
             </div>
         );
 
--- src/components/MobileModal/styledComponents.tsx
+++ src/components/MobileModal/styledComponents.tsx
@@ -2,13 +2,13 @@ import styled from "styled-components";
 
 export const Web3BrowserIconWrapper = styled.div`
     display: inline-block;
-    margin: 10px;
-`;
-
-export const Web3BrowserIconWrapperLeft = Web3BrowserIconWrapper.extend`
-    margin-left: 0;
+    margin: 20px;
 `;
 
 export const Web3BrowserIcon = styled.img`
     height: 36px;
+`;
+
+export const Web3BrowserIcons = styled.div`
+    text-align: center;
 `;
