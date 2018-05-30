5f663347a947d4068782d81d81260795d58d2d2c
Kayvon Tehranian <ktehranian@gmail.com>
2018-05-30 13:31:13 -0700

    feat: add styled components for displaying web3 browser icons

---
+++ src/components/MobileModal/styledComponents.tsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const Web3BrowserIconWrapper = styled.div`
+    display: inline-block;
+    margin: 10px;
+`;
+
+export const Web3BrowserIconWrapperLeft = Web3BrowserIconWrapper.extend`
+    margin-left: 0;
+`;
+
+export const Web3BrowserIcon = styled.img`
+    height: 36px;
+`;
